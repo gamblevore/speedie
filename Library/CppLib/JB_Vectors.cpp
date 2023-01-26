0d8ac2351d578baff9d2bed905169a5ee47d517e
gamblevore <theo@jeebox.org>
2023-01-26 10:49:09 +0000

    Update JB_Vectors.cpp

--- Library/CppLib/JB_Vectors.cpp
+++ Library/CppLib/JB_Vectors.cpp
@@ -12,40 +12,17 @@
 
 extern "C" {
 
-void cpuid(int CPUInfo[4], int InfoType) {
-    __asm__ __volatile__ (
-        "cpuid":
-        "=a" (CPUInfo[0]),
-        "=b" (CPUInfo[1]),
-        "=c" (CPUInfo[2]),
-        "=d" (CPUInfo[3]) :
-        "a" (InfoType), "c" (0)
-    );
-}
-
 bool SimdDetectAVX() {
-    int info[4];
-    cpuid(info, 0x00000001);
-    return (info[2] & ((int)1 << 28)) != 0;
+	return true; // assume true for now...
 }
-
 bool SimdDetectAVX2() {
-    int info[4];
-    cpuid(info, 0x00000001);
-    return (info[1] & ((int)1 <<  5)) != 0;
+	return true; // same
 }
-
-
 bool SimdDetectAVX512() {
-    int info[4];
-    cpuid(info, 0x00000001);
-    return (info[1] & ((int)1 << 16)) != 0;
+	return true; // same
 }
-
 bool SimdDetectSSE42() {
-    int info[4];
-    cpuid(info, 0x00000001);
-    return (info[2] & ((int)1 << 20)) != 0;
+	return true; // same
 }
 
 } // 
