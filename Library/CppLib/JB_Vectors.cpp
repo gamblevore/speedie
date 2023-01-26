2cfc2ff498c9de4144aba2d19a2205060e144cff
gamblevore <theo@jeebox.org>
2023-01-26 10:51:00 +0000

    Update JB_Vectors.cpp

--- Library/CppLib/JB_Vectors.cpp
+++ Library/CppLib/JB_Vectors.cpp
@@ -6,33 +6,6 @@
 	Copyright (C) 2013 Theodore H. Smith.
 */
 
-#ifndef AS_LIBRARY
-#include "JB_Umbrella.hpp"
-#include <cpuid.h>
-
-extern "C" {
-
-bool SimdDetectAVX() {
-	return true; // assume true for now...
-}
-bool SimdDetectAVX2() {
-	return true; // same
-}
-bool SimdDetectAVX512() {
-	return true; // same
-}
-bool SimdDetectSSE42() {
-	return true; // same
-}
-
-} // 
-
-#else
-
-// stop linker errors.
 extern "C" void Stub_JB_Vectors () {
-    
+	; // i guess this file does nothing now.
 }
-
-#endif
-
