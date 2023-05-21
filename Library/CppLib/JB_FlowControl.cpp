d8516ab3dcb29743b907300717500f21f240620f
gamblevore <theo@jeebox.org>
2023-05-21 10:45:03 +0100

    moved to speedie

--- Library/CppLib/JB_FlowControl.cpp
+++ Library/CppLib/JB_FlowControl.cpp
@@ -7,17 +7,6 @@
 
 
 extern "C" {
-	struct FlowControl;
-	extern FlowControl* JB__Flow_Flow;
-	extern uint  JB__Flow_Disabled;
-	void JB_Flow_Cond(FlowControl* flow, bool b);
-	bool JB_Flow__Cond(bool cond) {
-		if (!JB__Flow_Disabled) {
-			JB__Flow_Disabled++;
-			JB_Flow_Cond(JB__Flow_Flow, cond);
-			JB__Flow_Disabled--;
-		}
-		return cond;
-	}
+	// delete
 }
 
