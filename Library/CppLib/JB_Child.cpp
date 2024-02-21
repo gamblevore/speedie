b4f7aeb7a05fd33456e5c9ee50c669fafc1e2d69
gamblevore <theo@jeebox.org>
2024-02-21 12:18:53 +0000

    nil attempt #315

--- Library/CppLib/JB_Child.cpp
+++ Library/CppLib/JB_Child.cpp
@@ -86,7 +86,7 @@ void JB_CrashHandler(int Sig) {
 
 				// report to stdout
 	char ErrorBuff[128];
-	snprintf(ErrorBuff, sizeof(ErrorBuff), "%s Signal: %s\n", App_CallPath, strsignal(Sig));
+	snprintf(ErrorBuff, sizeof(ErrorBuff), "%s\nDate: %sSignal: %s\n", App_CallPath, ctime(0), strsignal(Sig));
 	fputs(ErrorBuff, stderr);
 	
 				// log to file
