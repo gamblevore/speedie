b2939256fbec4d983938713bdfbea0877b2e6005
gamblevore <theo@jeebox.org>
2024-03-06 21:58:50 +0000

    achieved

--- Library/CppLib/JB_Pico.cpp
+++ Library/CppLib/JB_Pico.cpp
@@ -1,7 +1,6 @@
 
 #define PICO_IMPLEMENTATION 
 #include "PicoMsg.h" // awesome new message system
-// should just have a "picoglobalconf" func... that gets a pointer to ALL the conf vars at once. 
 // :O 😇😮😵😇😮😵😇🤯😮😮🤯😮😇😮😵😇😮😮😇😮😵😮😮😮
 
 // AMAAZING
