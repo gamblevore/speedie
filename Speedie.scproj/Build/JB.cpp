efcd28fdf6d7c2219d333d2fd456c930a24134e9
gamblevore <theo@jeebox.org>
2024-05-31 00:31:47 +0100

    cleanup

--- Speedie.scproj/Build/JB.cpp
+++ Speedie.scproj/Build/JB.cpp
@@ -1927,7 +1927,7 @@ SCFunction* SC_Comp__LoadTypeTest(JB_String* S) {
 void SC_Comp__Main() {
 	if (SC_Comp__EnterCompile()) {
 		if (true) {
-			FlowControlStopper __varf1 = JB_Flow__FlowAllow(JB_LUB[1122], (112532581318656));
+			FlowControlStopper __varf1 = JB_Flow__FlowAllow(JB_LUB[1122], (112532609368064));
 			FlowControlStopper _usingf0 = JB_FlowControlStopper_SyntaxUsing(__varf1);
 			SC_Comp__CompileTime();
 			DTWrap* _tmPf2 = JB_Incr(JB_Wrap_ConstructorInt(nil, __varf1));
