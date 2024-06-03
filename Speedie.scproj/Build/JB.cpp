d0f26f1525e78f3a84bcdbb2431434404e77fa7e
gamblevore <theo@jeebox.org>
2024-06-03 13:04:07 +0100

    perry fixes

--- Speedie.scproj/Build/JB.cpp
+++ Speedie.scproj/Build/JB.cpp
@@ -1941,7 +1941,7 @@ SCFunction* SC_Comp__LoadTypeTest(JB_String* S) {
 void SC_Comp__Main() {
 	if (SC_Comp__EnterCompile()) {
 		if (true) {
-			FlowControlStopper __varf1 = JB_Flow__FlowAllow(JB_LUB[1122], (112552426050329));
+			FlowControlStopper __varf1 = JB_Flow__FlowAllow(JB_LUB[1122], (112552478275870));
 			FlowControlStopper _usingf0 = JB_FlowControlStopper_SyntaxUsing(__varf1);
 			SC_Comp__CompileTime();
 			DTWrap* _tmPf2 = JB_Incr(JB_Wrap_ConstructorInt(nil, __varf1));
--- jeebox.scproj/Cpp/JB.cpp
+++ jeebox.scproj/Cpp/JB.cpp
@@ -8362,7 +8362,7 @@ __lib__ int jb_shutdown() {
 }
 
 __lib__ int jb_version() {
-	return (2024060213);
+	return (2024060312);
 }
 
 __lib__ JB_String* jb_readfile(_cstring Path, bool AllowMissingFile) {
