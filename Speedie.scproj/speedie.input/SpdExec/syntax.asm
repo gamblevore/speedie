41ad24fcca020079773c5483f01d3064158c8c9a
gamblevore <theo@jeebox.org>
2024-07-26 00:24:36 +0100

    asm :D

--- Speedie.scproj/speedie.input/SpdExec/syntax.asm
+++ Speedie.scproj/speedie.input/SpdExec/syntax.asm
@@ -2,11 +2,12 @@
 
 tests oofit
 	asm oofit (0)
+		KNST  (r1, 18874368)
 		KNST  (r1, 3458764513820540931)
 		KNST  (r1, 702)			// 123
 		KNST  (r4, 2)
 		@test
-		KNST  (r3, 18874368)
+		KNST  (r3, 555)
 		KNST  (r3, -234)
 		ADD   (r1, r1, r3, 0)
 		PRNT  (r1,)				// -111
