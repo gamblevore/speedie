27b08b995225661864a485923c15f26c40501fc0
gamblevore <theo@jeebox.org>
2024-07-30 17:05:15 +0100

    cleanup

--- Library/CppLib/JB_Temporal.cpp
+++ Library/CppLib/JB_Temporal.cpp
@@ -3,7 +3,7 @@
 //  Generate temporal number stream: © 2019-2020 Theodore H. Smith
 //  Could be used in almost anything! Even games :3
 //  Can we make a computer FEEL Psychic energy?
-// 
+//  2025: I might remove some of this? What if we can just get temporal info via the VM?
 
 #include "JB_BasicTypes.h"
 #include "JB_Temporal.h"
@@ -11,30 +11,6 @@
 #ifdef __APPLE__
 	#include <mach/mach_time.h>
 #endif
-
-/*
-#define FOR_(var, count)	for (int var = 0; var < (count); var++)
-#define require(expr)		if (!(expr)) {return {};}
-#define New(x)				std::make_shared<x>()
-#define New2(x,a)			std::make_shared<x>(a)
-#define New3(x,a,b)			std::make_shared<x>(a,b)
-#define New4(x,a,b,c)		std::make_shared<x>(a,b,c)
-#define Now()				std::chrono::high_resolution_clock::now()
-#define self				(*this)
-#define AlwaysInline		inline __attribute__((__always_inline__))
-#define ChronoLength(Start)	(std::chrono::duration_cast<std::chrono::duration<float>>(Now() - Start).count())
-#if defined(DEBUG) && !defined(__ANDROID__)
-	#define debugger asm("int3")
-	#define DEBUG_AS_NUM 1
-#else
-	#define debugger
-	#define DEBUG_AS_NUM 0
-#endif
-#define Ooof				[[maybe_unused]] static
-#define test(cond)			if (!(cond)) {debugger;}
-#define sizecheck(a,b)		static_assert (sizeof(a)==b, "bad size!") // sizecheck
-#define     ArgError			-5555
- */
 
 #define for_(count)			for (int i = 0; i < (count); i++)
 typedef u32					uSample;
