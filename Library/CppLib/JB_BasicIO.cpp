342c5453a1144c00f0c524deed93d4a319e8223b
gamblevore <theo@jeebox.org>
2024-11-27 22:55:21 +0000

    colors

--- Library/CppLib/JB_BasicIO.cpp
+++ Library/CppLib/JB_BasicIO.cpp
@@ -34,18 +34,18 @@ void JB_Str_Print(JB_String* S) {
 		SendToStdOut(S->Addr, S->Length);
 }
 
-FastString* PrintCapturer;
-FastString** JB_Str_PrintCapturer() {
-	return &PrintCapturer;
-}
+//FastString* PrintCapturer;
+//FastString** JB_Str_PrintCapturer() {
+//	return &PrintCapturer;
+//}
 
 void JB_Str_PrintLine(JB_String* S) {
 	JB_Str_Print(S);
 	SendToStdOut( (uint8*)"\n", 1 );
-	auto P = PrintCapturer;
-	if_rare (P) {
-		JB_FS_AppendLine(P, S);
-	}
+//	auto P = PrintCapturer;
+//	if_rare (P) {`
+//		JB_FS_AppendLine(P, S);
+//	}
 }
 
 #else
