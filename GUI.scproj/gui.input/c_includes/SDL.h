5e955c31c00ac7fe79ab86fc10ae4e4b0512ad81
gamblevore <theo@jeebox.org>
2025-06-18 23:49:29 +0100

    Added adaptive cursor icons

--- GUI.scproj/gui.input/c_includes/SDL.h
+++ GUI.scproj/gui.input/c_includes/SDL.h
@@ -7,9 +7,16 @@ typedef int SDL_Scancode;
 #define SDL_keycode_h_
 #include <SDL2/SDL.h>
 #undef __CC_ARM
+
 struct SDL_SysWMinfo;
 extern "C" {
 extern DECLSPEC SDL_bool SDLCALL SDL_GetWindowWMInfo(SDL_Window* window, SDL_SysWMinfo* info);
+
+// enums suck so bad. they make everything uncompileable!
+inline SDL_Cursor* JB_CreateSystemCursor (int ID) {
+	return SDL_CreateSystemCursor((SDL_SystemCursor)ID);
+}
+
 }
 
 
