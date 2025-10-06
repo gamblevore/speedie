df0385deb70bb52e5404291ae58045f20aa5a3c7
gamblevore <theo@jeebox.org>
2025-10-06 17:10:47 +0100

    Update JB_Run.cpp

--- Library/CppLib/JB_Run.cpp
+++ Library/CppLib/JB_Run.cpp
@@ -168,7 +168,9 @@ void JB_FinalEvents() {
 	AddError(JB_Rec_ShellPrintErrors(nil),	"jb.stderr");
 	JB_LibShutdown();
 	JB_RemoveHandlers(); // some wierd systems call signals after we exit??
+	#ifdef AS_LIBRARY
 	JB_KillChildrenOnExit();
+	#endif
 }
 
 
